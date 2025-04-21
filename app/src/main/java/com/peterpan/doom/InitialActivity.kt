635332e1b4000eebd5858c47172b6d2335a585cc
Luiz Valdetaro <luizclaudio@valdetaro.com>
2025-04-21 12:32:18 -0500

    dependency upgrades

--- app/src/main/java/com/peterpan/doom/InitialActivity.kt
+++ app/src/main/java/com/peterpan/doom/InitialActivity.kt
@@ -3,28 +3,35 @@ package com.peterpan.doom
 import android.content.Intent
 import androidx.activity.compose.setContent
 import android.os.Bundle
-import club.gepetto.circum.CircumActivity
+import androidx.activity.ComponentActivity
+import androidx.activity.enableEdgeToEdge
+import club.gepetto.circum.circumIntentProcessor
 import com.peterpan.doom.ui.GameLoadingState
 
-open class InitialActivity : CircumActivity<Any>() {
+open class InitialActivity : ComponentActivity() {
+    private lateinit var dIp : DoomIntentProcessor
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setIntentProcessor(DoomIntentProcessor::class.java)
+        enableEdgeToEdge()
     }
 
-    override fun onStateUpdate(state: Any) {
-        super.onStateUpdate(state)
-        
-        when (state) {
-            is DoomState.Loading -> {
-                setContent { GameLoadingState() }
-            }
-            is DoomState.Loaded -> {
-                App.gameInstalled = true
-                val intent = Intent(this, DoomClientActivity::class.java)
-                intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
-                this.startActivity(intent)
+    override fun onResume() {
+        super.onResume()
+
+        dIp = circumIntentProcessor<DoomIntentProcessor>(this)
+
+        dIp.onStateUpdated{ state ->
+            when (state) {
+                is DoomState.Loading -> {
+                    setContent { GameLoadingState() }
+                }
+                is DoomState.Loaded -> {
+                    App.gameInstalled = true
+                    val intent = Intent(this, DoomClientActivity::class.java)
+                    intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+                    this.startActivity(intent)
+                }
             }
         }
     }
